0f2ddc1062053408cd8142295b8ecf1048bbdbd9
Madhav Singh <88900164+madhav3108@users.noreply.github.com>
2026-01-07 22:39:31 +0530

    prod change

--- src/test/java/com/example/cric_info/CricInfoApplicationTests.java
+++ src/test/java/com/example/cric_info/CricInfoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.cric_info;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CricInfoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.cric_info;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CricInfoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
